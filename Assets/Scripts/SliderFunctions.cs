313be8723f8d65f5ab56f0c8a781f94df8c76ea0
HappyChaos1126 <matthewramey69@gmail.com>
2025-11-23 17:30:26 -0600

    Audio slider bugs
    Bug for muted sound when muting music, and sliders not saving volume between scenes

--- Assets/Scripts/SliderFunctions.cs
+++ Assets/Scripts/SliderFunctions.cs
@@ -1,18 +1,50 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class SliderFunctions : MonoBehaviour
 {
+    [Header("Audio Mixer")]
+    [SerializeField] AudioMixer mixer;
+
+    [Header("Audio Sliders")]
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider sfxSlider;
+
+    public void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume"))
+        { LoadVolume(); }
+        else
+        {
+            changeMusic(musicSlider);
+            changeSound(sfxSlider);
+        }
+    }
+
     public void changeSound(Slider input)
     {
-        AudioManager.instance.soundVolume = input.value;
-        AudioManager.instance.SaveSound();
+        //AudioManager.instance.soundVolume = input.value;
+        //AudioManager.instance.SaveSound();
+        float volume = sfxSlider.value;
+        mixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
     }
     public void changeMusic(Slider input)
     {
-        AudioManager.instance.musicVolume = input.value;
-        AudioManager.instance.SaveMusic();
+        //AudioManager.instance.musicVolume = input.value;
+        //AudioManager.instance.SaveMusic();
+        float volume = musicSlider.value;
+        mixer.SetFloat("Music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
 
+    private void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        changeMusic(musicSlider);
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        changeSound(sfxSlider);
     }
 
 }
